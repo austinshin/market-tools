65dabf12933f4c1f981062f45278552f2e22e77d
austinshin <shinaustin@gmail.com>
2024-05-12 14:04:16 -0700

    add share size calculator

--- src/components/ShareSizeCalculator.jsx
+++ src/components/ShareSizeCalculator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Container from 'react-bootstrap/Container';
@@ -10,9 +10,37 @@ import Button from 'react-bootstrap/Button';
 const ShareSizeCalculator = (props) => {
 
   const { accountInfo } = useSelector(state => state?.accountInfo);
+  const [shareSize, setShareSize] = useState(0);
+  const [formData, setFormData] = useState({
+    accountBalance: 0,
+    risk: 1,
+    entry: 1.05,
+    stopLoss: 1.10,
+    rewardRisk: 1,
+    fees: 0,
+    slippage: 0,
+  });
+
+  useEffect(() => {
+    setFormData({ ...formData, ['accountBalance']: accountInfo.accountBalance })
+  }, [accountInfo])
 
   const handleChange = (e) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
 
+  const calculate = (e) => {
+    console.log('formData', formData);
+    const { accountBalance, risk, entry, stopLoss, rewardRisk, fees, slippage } = formData;
+    const moneyAtRisk = accountBalance * risk / 100;
+
+    let tradeSize = moneyAtRisk / (entry - stopLoss);
+    console.log('moneyatRisk', moneyAtRisk)
+    console.log('entry', entry)
+    console.log('stop loss', stopLoss);
+    console.log('tradeSize', tradeSize);
+    setShareSize(tradeSize);
   }
 
   return (
@@ -27,8 +55,9 @@ const ShareSizeCalculator = (props) => {
             placeholder="Account Balance size"
             aria-label="Account Balance"
             aria-describedby="basic-addon1"
-            type="text"
-            value={accountInfo.accountBalance}
+            type="number"
+            value={formData.accountBalance}
+            name="accountBalance"
             onChange={handleChange}
           >
           </Form.Control>
@@ -37,41 +66,73 @@ const ShareSizeCalculator = (props) => {
             placeholder="Risk %"
             aria-label="Risk"
             aria-describedby="basic-addon1"
+            type="number"
+            value={formData.risk}
+            name="risk"
+            onChange={handleChange}
           />
-          <InputGroup.Text id="basic-addon1">Entry ($)</InputGroup.Text>
-          <Form.Control
-            placeholder="Entry"
-            aria-label="Entry"
-            aria-describedby="basic-addon1"
-          />
-          <InputGroup.Text id="basic-addon1">Stop Loss ($)</InputGroup.Text>
-          <Form.Control
-            placeholder="Stop Loss"
-            aria-label="Stop Loss"
-            aria-describedby="basic-addon1"
-          />
+
           <InputGroup.Text id="basic-addon1">Reward/Risk</InputGroup.Text>
           <Form.Control
             placeholder="Reward/Risk"
             aria-label="Reward/Risk"
             aria-describedby="basic-addon1"
+            type="number"
+            value={formData.rewardRisk}
+            name="rewardRisk"
+            onChange={handleChange}
           />
           <InputGroup.Text id="basic-addon1">Fees (%)</InputGroup.Text>
           <Form.Control
             placeholder="Fees"
             aria-label="Fees"
             aria-describedby="basic-addon1"
+            type="number"
+            value={formData.fees}
+            name="fees"
+            onChange={handleChange}
           />
           <InputGroup.Text id="basic-addon1">Slippage (%)</InputGroup.Text>
           <Form.Control
             placeholder="Slippage"
             aria-label="Slippage"
             aria-describedby="basic-addon1"
+            type="number"
+            value={formData.slippage}
+            name="slippage"
+            onChange={handleChange}
           />
-          <Button className="share-size-calculator-submit" variant="success">Calculate</Button>
+          <InputGroup.Text id="basic-addon1">Entry ($)</InputGroup.Text>
+          <Form.Control
+            placeholder="Entry"
+            aria-label="Entry"
+            aria-describedby="basic-addon1"
+            type="number"
+            value={formData.entry}
+            name="entry"
+            onChange={handleChange}
+          />
+          <InputGroup.Text id="basic-addon1">Stop Loss ($)</InputGroup.Text>
+          <Form.Control
+            placeholder="Stop Loss"
+            aria-label="Stop Loss"
+            aria-describedby="basic-addon1"
+            type="number"
+            value={formData.stopLoss}
+            name="stopLoss"
+            onChange={handleChange}
+          />
+          <Button className="share-size-calculator-submit" variant="success" onClick={calculate}>Calculate</Button>
 
         </div>
-        <div className="p-2">2</div>
+        <div className="p-2">
+          <div>
+            {shareSize} shares
+          </div>
+          <div>
+            Profit target
+          </div>
+        </div>
         <div className="p-2">3</div>
       </Stack>
 
